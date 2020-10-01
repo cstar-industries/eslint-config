b129c76b0e2d8b4dbe0e1246e63f51d34f7355e8
Charles Francoise <charles.francoise@gmail.com>
2020-10-01 11:05:58 +0200

    first import

---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'plugin:import/errors', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    eqeqeq: ['error', 'always'],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+    'curly': ['error', 'all'],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-unused-vars': [
+      'error',
+      // Ignore no-unused-vars for variables named only with underscores: `_`, `__`, etc.
+      {
+        varsIgnorePattern: '^_+$',
+        argsIgnorePattern: '^_+$',
+        caughtErrorsIgnorePattern: '^_+$'
+      }
+    ],
+    'import/no-unresolved': ['off'],
+    'import/first': ['error'],
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'parent'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ],
+    'import/newline-after-import': ['warn']
+  }
+};
